d7fb8dc5791b7674fd9f6b5e73d37de2e2eea4a9
Daniel Miller <danielmiller5791@gmail.com>
2018-03-26 17:34:59 +0100

    simplecov & rubocop committed

--- lib/app.rb
+++ lib/app.rb
@@ -1,12 +1,3 @@
 require_relative 'card'
 require_relative 'fare'
 require_relative 'station'
-
-
-card = Card.new
-card.top_up(66)
-card.balance
-card.top_up(22)
-card.balance
-card.deduct
-card.balance
